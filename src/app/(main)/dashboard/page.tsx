8e46c9eed6e11456ce44c17256fcbc3191736fe1
Thilina Ruwan <thilinaruwan112@gmail.com>
2025-07-22 19:19:37 +0000

    Use 2 column grid to dashboard counters

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -76,7 +76,7 @@ export default function Dashboard() {
           </Button>
         </div>
       </div>
-      <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
+      <div className="grid gap-4 sm:grid-cols-2">
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Total Stock</CardTitle>
