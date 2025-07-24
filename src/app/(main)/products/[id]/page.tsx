e0d80767c8ac2b8045fee3867f6e7c0f80e108fc
Thilina Ruwan <thilinaruwan112@gmail.com>
2025-07-24 08:30:28 +0000

    yes confirm

--- src/app/(main)/products/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-
-import { EditProductForm } from '@/components/edit-product-form';
-import { use } from 'react';
-
-export default function EditProductPage({ params }: { params: Promise<{ id: string }> }) {
-  const { id } = use(params);
-
-  return <EditProductForm id={id} />;
-}
