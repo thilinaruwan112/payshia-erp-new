68dde503a4d3147d13727e0c45427ace82aae472
Thilina Ruwan <thilinaruwan112@gmail.com>
2025-07-24 08:35:55 +0000

    please debug this code and show the params id in the console

--- src/app/(main)/products/[id]/page.tsx
+++ src/app/(main)/products/[id]/page.tsx
@@ -4,5 +4,6 @@ import { EditProductForm } from '@/components/edit-product-form';
 export default async function EditProductPage({ params }: { params: { id: string } }) {
   // We can directly pass the id to the client component
   // which will handle the data fetching.
+  console.log('--- DEBUG: params.id ---', params.id);
   return <EditProductForm id={params.id} />;
 }
