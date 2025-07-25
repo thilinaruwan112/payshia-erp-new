2a0a633742cfe96eae245d711954d6982c7c7abf
Thilina Ruwan <thilinaruwan112@gmail.com>
2025-07-25 07:56:04 +0000

    why they are not in same line

--- src/components/purchase-order-form.tsx
+++ src/components/purchase-order-form.tsx
@@ -129,7 +129,7 @@ export function PurchaseOrderForm({ suppliers, products }: PurchaseOrderFormProp
             <CardHeader>
                 <CardTitle>PO Details</CardTitle>
             </CardHeader>
-            <CardContent className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+            <CardContent className="grid grid-cols-1 md:grid-cols-4 gap-6">
                 <FormField
                     control={form.control}
                     name="supplierId"
