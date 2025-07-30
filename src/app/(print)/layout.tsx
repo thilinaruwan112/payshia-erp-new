cf7fa463c3d7874d478013df4e810f51ed1065ae
Thilina Ruwan <thilinaruwan112@gmail.com>
2025-07-30 03:29:41 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(print)/layout.tsx
+++ src/app/(print)/layout.tsx
@@ -12,20 +12,10 @@ export default function PrintLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:400,500,600,700&display=swap"
-          rel="stylesheet"
-        ></link>
-      </head>
-      <body className="print:bg-white print:text-black bg-gray-100 dark:bg-gray-800">
-        <main className="flex justify-center">
-            {children}
-        </main>
-      </body>
-    </html>
+    <div className="print:bg-white print:text-black bg-gray-100 dark:bg-gray-800 font-[Poppins]">
+      <main className="flex justify-center">
+        {children}
+      </main>
+    </div>
   );
 }
