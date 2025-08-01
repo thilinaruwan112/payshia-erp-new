d033cf43f2b87c673540899c04fd2554c6e8b15b
Yomal theekshana <yomaltheekshana66@gmail.com>
2025-08-01 08:53:01 +0000

    no need to to open date just need to show the today date

--- src/components/receipt-form.tsx
+++ src/components/receipt-form.tsx
@@ -378,39 +378,11 @@ export function ReceiptForm({ customers }: ReceiptFormProps) {
                             control={form.control}
                             name="date"
                             render={({ field }) => (
-                                <FormItem className="flex flex-col">
+                                <FormItem>
                                 <FormLabel>Date of Payment</FormLabel>
-                                <Popover>
-                                    <PopoverTrigger asChild>
-                                    <FormControl>
-                                        <Button
-                                        variant={"outline"}
-                                        className={cn(
-                                            "w-full pl-3 text-left font-normal",
-                                            !field.value && "text-muted-foreground"
-                                        )}
-                                        >
-                                        {field.value ? (
-                                            format(field.value, "PPP")
-                                        ) : (
-                                            <span>Pick a date</span>
-                                        )}
-                                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                                        </Button>
-                                    </FormControl>
-                                    </PopoverTrigger>
-                                    <PopoverContent className="w-auto p-0" align="start">
-                                    <Calendar
-                                        mode="single"
-                                        selected={field.value}
-                                        onSelect={field.onChange}
-                                        disabled={(date) =>
-                                        date > new Date() || date < new Date("1900-01-01")
-                                        }
-                                        initialFocus
-                                    />
-                                    </PopoverContent>
-                                </Popover>
+                                <FormControl>
+                                    <Input readOnly value={format(field.value, "PPP")} />
+                                </FormControl>
                                 <FormMessage />
                                 </FormItem>
                             )}
