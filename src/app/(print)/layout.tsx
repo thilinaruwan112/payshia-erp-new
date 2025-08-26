bf15c54f09ec84fd9f206541201bba927ad722ad
Thilina Ruwan <thilinaruwan112@gmail.com>
2025-08-26 04:06:50 +0000

    but keep html2canvas

--- src/app/(print)/layout.tsx
+++ src/app/(print)/layout.tsx
@@ -15,7 +15,6 @@ export default function PrintLayout({
   return (
     <html lang="en">
       <head>
-          <Script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js" strategy="beforeInteractive"></Script>
           <Script src="https://unpkg.com/jsprintmanager/JSPrintManager.js" strategy="beforeInteractive" />
       </head>
       <body className="print:bg-white print:text-black bg-gray-100 dark:bg-gray-800 font-[Poppins]">
