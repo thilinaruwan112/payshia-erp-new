c4a179646dfd7eade9bd3d88d9f30e520805cbe4
Thilina Ruwan <thilinaruwan112@gmail.com>
2025-08-27 12:16:29 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/app-shell.tsx
+++ src/components/app-shell.tsx
@@ -457,7 +457,7 @@ function UserMenu({ user }: { user: any }) {
 function Brand({ companyName }: { companyName: string }) {
   return (
     <Link href="/dashboard" className="flex items-center gap-2 font-bold text-lg">
-      <Image src="https://content-provider.payshia.com/payshia-erp/branding/Transparent-01u.png" alt="Payshia ERP Logo" width={32} height={32} />
+      <Image src="https://content-provider.payshia.com/payshia-erp/branding/Transparent-01u.png" alt="Payshia ERP Logo" width={40} height={40} />
     </Link>
   );
 }
