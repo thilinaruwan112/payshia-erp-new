0a03cdeedb129f246dd7fbb85048453c9aa3cf54
Thilina Ruwan <thilinaruwan112@gmail.com>
2025-08-27 06:58:15 +0000

    use this dialog box full screen

--- src/components/pos/dialogs/new-order-dialog.tsx
+++ src/components/pos/dialogs/new-order-dialog.tsx
@@ -121,7 +121,7 @@ export function NewOrderDialog({ isOpen, onOpenChange, activeOrders = [], create
 
   return (
     <Dialog open={isOpen} onOpenChange={onOpenChange}>
-      <DialogContent className="max-w-4xl">
+      <DialogContent className="max-w-full h-full">
         <DialogHeader>
           <DialogTitle className="text-2xl">Create New Order</DialogTitle>
           <DialogDescription>Select an order type or choose a table for dine-in.</DialogDescription>
