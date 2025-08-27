96f61913053efc3a81f5034571eac4a84989bcc4
Yomal theekshana <yomaltheekshana66@gmail.com>
2025-08-27 10:15:54 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -17,7 +18,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'http',
+        protocol: 'https',
         hostname: 'content-provider.payshia.com',
         port: '',
         pathname: '/**',
