604699f3209ccb6b01dab4b92e7551c010f04d98
Thilina Ruwan <thilinaruwan112@gmail.com>
2025-08-31 16:18:26 +0000

    use 70% height and 70% width for deskopmode

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -40,8 +40,8 @@ const DialogContent = React.forwardRef<
       ref={ref}
       className={cn(
         "fixed z-50 grid w-full gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95",
-        "inset-x-0 bottom-0 h-full data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom",
-        "sm:left-[50%] sm:top-[50%] sm:h-auto sm:w-full sm:max-w-2xl sm:translate-x-[-50%] sm:translate-y-[-50%] sm:rounded-lg sm:data-[state=closed]:slide-out-to-top-[48%] sm:data-[state=open]:slide-in-from-top-[48%]",
+        "inset-0 h-full", // Full screen on mobile
+        "sm:left-[50%] sm:top-[50%] sm:h-[70vh] sm:w-[70vw] sm:translate-x-[-50%] sm:translate-y-[-50%] sm:rounded-lg sm:data-[state=closed]:slide-out-to-top-[48%] sm:data-[state=open]:slide-in-from-top-[48%]", // Desktop styles
         className
       )}
       {...props}
