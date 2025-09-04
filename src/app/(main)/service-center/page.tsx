87bf838ad9f203b82a6537730e94049a78cb9078
Thilina Ruwan <thilinaruwan112@gmail.com>
2025-09-04 07:42:38 +0000

    All 3 options are loaded same page why 3 buttons then?

--- src/app/(main)/service-center/page.tsx
+++ src/app/(main)/service-center/page.tsx
@@ -137,12 +137,6 @@ export default function ServiceCenterPage() {
                         <DropdownMenuItem asChild>
                            <Link href={`/service-center/jobs/${job.id}`}>View/Edit Job</Link>
                         </DropdownMenuItem>
-                         <DropdownMenuItem asChild>
-                           <Link href={`/service-center/jobs/${job.id}`}>Add Technician Report</Link>
-                        </DropdownMenuItem>
-                         <DropdownMenuItem asChild>
-                           <Link href={`/service-center/jobs/${job.id}`}>Create Invoice</Link>
-                        </DropdownMenuItem>
                       </DropdownMenuContent>
                     </DropdownMenu>
                   </TableCell>
