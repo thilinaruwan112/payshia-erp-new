1290368871786f99c189b6b8676d844c4cdd9b4e
Thilina Ruwan <thilinaruwan112@gmail.com>
2025-10-26 17:56:57 +0000

    for the claculated height need to get only this id height id="receipt-pr

--- src/app/(print)/layout.tsx
+++ src/app/(print)/layout.tsx
@@ -9,12 +9,9 @@ export default function PrintLayout({
 }>) {
   return (
       <CurrencyProvider>
-        <main className="flex justify-center font-[Poppins] print:bg-white print:text-black">
+        <main className="flex items-center h-[100vh] justify-center font-[Poppins] print:bg-white print:text-black">
             {children}
         </main>
       </CurrencyProvider>
-      //  <main className="flex justify-center font-[Poppins] print:bg-white print:text-black">
-      //  {children}
-      // </main>
   );
 }
